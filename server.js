65451b504df20c056f7eeefc2da190f70742fba8
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-07-26 13:59:47 +0100

    Revert "Server: Add full path for local testing"
    This reverts commit 186539a68310e474579bfe8d9de1dabb40cd8c65.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'http://localhost:3000/dashboard/settings', 'https://portfolio-ravenous.herokuapp.com'],
+    originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
