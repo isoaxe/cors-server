8951546f99315813e16e5b547c4bd642f7b3b072
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-05 21:27:44 +0700

    index: Correct faulty server declaration
    Need to assign the server to a variable and then call emit on that.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const corsAnywhere = require('cors-anywhere');
 
-corsAnywhere.createServer({
+const corsServer = corsAnywhere.createServer({
     originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
@@ -9,5 +9,6 @@ corsAnywhere.createServer({
 
 exports.proxy = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
+  corsServer.emit('request', request, response);
   response.send("Hello from Firebase!");
 });
