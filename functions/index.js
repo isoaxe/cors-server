9c491f4d4e0857748dbf90838695b9db0a3073d7
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-05 20:53:01 +0700

    index: Create server with cors anywhere
    Same as in server.js, except don't listen at port and host.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,11 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const corsAnywhere = require('cors-anywhere');
+
+corsAnywhere.createServer({
+    originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com'],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+});
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
