157390b3594d0b25997259fe2af1f7c195cbd2b3
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-07 10:08:57 +0700

    index: Add urls to whitelist
    + Add both urls for the instapic project.
    + Reduce line length by having only one address per line in the whitelist array.

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,13 @@ const functions = require('firebase-functions');
 const corsAnywhere = require('cors-anywhere');
 
 const corsServer = corsAnywhere.createServer({
-    originWhitelist: ['http://localhost:3000', 'http://localhost:5000', 'https://portfolio-ravenous.herokuapp.com'],
+    originWhitelist: [
+      'http://localhost:3000',
+      'http://localhost:5000',
+      'https://portfolio-ravenous.herokuapp.com',
+      'https://insta-profile-pic.firebaseapp.com',
+      'https://insta-profile-pic.web.app'
+    ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 });
