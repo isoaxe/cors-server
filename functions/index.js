2c9f19c10a2ec95b902469afc12e788867c437ca
Isaac <isaackoz130@gmail.com>
2022-10-27 20:42:01 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,9 @@
 const {onRequest} = require("firebase-functions/v2/https");
-const corsAnywhere = require('cors-anywhere');
 const cors = require("cors")({origin:true})
-const corsServer = corsAnywhere.createServer({
-    originWhitelist: [
-      'http://localhost:3000',
-    ],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
-});
 
 exports.proxy = onRequest({cors:'http://localhost:3000'},(request, response) => {
   response.set('Access-Control-Allow-Origin', '*');
   cors(request,response,() =>{
-    corsServer.emit('request', request, response);
+    // function goes here
   })    
 });
