a53062087b7eaed0b50629c2309db7b617f6fd91
Isaac <isaackoz130@gmail.com>
2022-10-27 20:36:33 -0700

    fixed cors issue on firebase end

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,17 @@
-const functions = require('firebase-functions');
+const {onRequest} = require("firebase-functions/v2/https");
 const corsAnywhere = require('cors-anywhere');
-
+const cors = require("cors")({origin:true})
 const corsServer = corsAnywhere.createServer({
     originWhitelist: [
       'http://localhost:3000',
-      'http://localhost:5000',
-      'https://insta-profile-pic.firebaseapp.com',
-      'https://insta-profile-pic.web.app',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com/',
-      'https://portfolio-ravenous.herokuapp.com'
     ],
     requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    removeHeaders: ['cookie', 'cookie2'],
 });
 
-exports.proxy = functions.https.onRequest((request, response) => {
+exports.proxy = onRequest({cors:'http://localhost:3000'},(request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  cors(request,response,() =>{
     corsServer.emit('request', request, response);
+  })    
 });
