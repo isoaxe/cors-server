4efde7ec7f2e3d2a8f94621fa0fc28398b2f5592
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-29 13:54:13 +0700

    index: Remove unused whitelisted urls
    Tidy up whitelist by removing unused origin urls.

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,8 @@ const corsServer = corsAnywhere.createServer({
     originWhitelist: [
       'http://localhost:3000',
       'http://localhost:5000',
-      'https://insta-profile-pic.firebaseapp.com',
-      'https://insta-profile-pic.web.app',
       'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com/',
-      'https://portfolio-ravenous.herokuapp.com'
+      'https://portfolio-ravenous.firebaseapp.com'
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
